90e3f671ceb82bc440ec0bc65274193be636b103
Lenni0451 <20379977+Lenni0451@users.noreply.github.com>
2024-07-26 18:46:40 +0200

    Defined annotations

---
+++ src/main/java/net/lenni0451/optconfig/annotations/Option.java
@@ -0,0 +1,14 @@
+package net.lenni0451.optconfig.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Option {
+
+    String value() default "";
+
+}
