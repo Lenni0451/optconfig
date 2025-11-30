d7fa1182d3f06100a0ce96bcca89b0081f9cc3e7
Lenni0451 <20379977+Lenni0451@users.noreply.github.com>
2025-11-30 20:36:26 +0100

    Add more javadoc

--- src/main/java/net/lenni0451/optconfig/serializer/info/DeserializerInfo.java
+++ src/main/java/net/lenni0451/optconfig/serializer/info/DeserializerInfo.java
@@ -21,10 +21,33 @@ import java.lang.reflect.Type;
 public record DeserializerInfo<T>(@Nullable Object configInstance, TypeSerializerList typeSerializers, Class<T> type, @Nullable Type genericType, T currentValue,
         Object serializedValue) {
 
+    /**
+     * Derive a new deserializer info with unchecked casts.<br>
+     * <b>Use with caution!</b>
+     *
+     * @param type            The type
+     * @param genericType     The generic type
+     * @param currentValue    The current value
+     * @param serializedValue The serialized value
+     * @param <O>             The type of the config option
+     * @return The derived deserializer info
+     * @see #derive(Class, Type, Object, Object)
+     */
     public <O> DeserializerInfo<O> uncheckedDerive(final Class<?> type, @Nullable final Type genericType, final Object currentValue, final Object serializedValue) {
         return this.derive((Class<O>) type, genericType, (O) currentValue, serializedValue);
     }
 
+    /**
+     * Derive a new deserializer info.<br>
+     * This creates a new deserializer info with the given parameters, while keeping the config instance and type serializers the same.
+     *
+     * @param type            The type
+     * @param genericType     The generic type
+     * @param currentValue    The current value
+     * @param serializedValue The serialized value
+     * @param <O>             The type of the config option
+     * @return The derived deserializer info
+     */
     public <O> DeserializerInfo<O> derive(final Class<O> type, @Nullable final Type genericType, final O currentValue, final Object serializedValue) {
         return new DeserializerInfo<>(this.configInstance, this.typeSerializers, type, genericType, currentValue, serializedValue);
     }
--- src/main/java/net/lenni0451/optconfig/serializer/info/SerializerInfo.java
+++ src/main/java/net/lenni0451/optconfig/serializer/info/SerializerInfo.java
@@ -9,10 +9,31 @@ import java.lang.reflect.Type;
 @With
 public record SerializerInfo<T>(@Nullable Object configInstance, TypeSerializerList typeSerializers, Class<T> type, @Nullable Type genericType, T value) {
 
+    /**
+     * Derive a new serializer info with unchecked casts.<br>
+     * <b>Use with caution!</b>
+     *
+     * @param type        The type
+     * @param genericType The generic type
+     * @param value       The value
+     * @param <O>         The type of the config option
+     * @return The derived serializer info
+     * @see #derive(Class, Type, Object)
+     */
     public <O> SerializerInfo<O> uncheckedDerive(final Class<?> type, @Nullable final Type genericType, final Object value) {
         return this.derive((Class<O>) type, genericType, (O) value);
     }
 
+    /**
+     * Derive a new serializer info.<br>
+     * This creates a new serializer info with the given parameters, while keeping the config instance and type serializers the same.
+     *
+     * @param type        The type
+     * @param genericType The generic type
+     * @param value       The value
+     * @param <O>         The type of the config option
+     * @return The derived serializer info
+     */
     public <O> SerializerInfo<O> derive(final Class<O> type, @Nullable final Type genericType, final O value) {
         return new SerializerInfo<>(this.configInstance, this.typeSerializers, type, genericType, value);
     }
