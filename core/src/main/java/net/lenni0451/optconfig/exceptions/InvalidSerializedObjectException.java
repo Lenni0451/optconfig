4172e32a426246bb191e3789276044c635303500
Lenni0451 <20379977+Lenni0451@users.noreply.github.com>
2026-03-27 21:17:05 +0100

    Allow the actual type to be null

--- core/src/main/java/net/lenni0451/optconfig/exceptions/InvalidSerializedObjectException.java
+++ core/src/main/java/net/lenni0451/optconfig/exceptions/InvalidSerializedObjectException.java
@@ -2,14 +2,16 @@ package net.lenni0451.optconfig.exceptions;
 
 import net.lenni0451.optconfig.serializer.ConfigTypeSerializer;
 
+import javax.annotation.Nullable;
+
 /**
  * An exception that is thrown when a serialized object is invalid.<br>
  * This exception can be used in custom {@link ConfigTypeSerializer}s.
  */
 public class InvalidSerializedObjectException extends RuntimeException {
 
-    public InvalidSerializedObjectException(final Class<?> expected, final Class<?> actual) {
-        super("Invalid serialized object, expected " + expected.getName() + " but got " + actual.getName());
+    public InvalidSerializedObjectException(final Class<?> expected, @Nullable final Class<?> actual) {
+        super("Invalid serialized object, expected " + expected.getName() + " but got " + (actual == null ? "null" : actual.getName()));
     }
 
 }
