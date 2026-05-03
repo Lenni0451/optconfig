dc29479c8b042ccb0235b46b6de3ae8e3a7eb629
Lenni0451 <20379977+Lenni0451@users.noreply.github.com>
2026-05-03 22:46:56 +0200

    Added getter for types

--- core/src/main/java/net/lenni0451/optconfig/exceptions/InvalidSerializedObjectException.java
+++ core/src/main/java/net/lenni0451/optconfig/exceptions/InvalidSerializedObjectException.java
@@ -1,5 +1,6 @@
 package net.lenni0451.optconfig.exceptions;
 
+import lombok.Getter;
 import net.lenni0451.optconfig.serializer.ConfigTypeSerializer;
 
 import javax.annotation.Nullable;
@@ -8,7 +9,12 @@ import javax.annotation.Nullable;
  * An exception that is thrown when a serialized object is invalid.<br>
  * This exception can be used in custom {@link ConfigTypeSerializer}s.
  */
+@Getter
 public class InvalidSerializedObjectException extends RuntimeException {
+
+    private final Class<?> expectedType;
+    @Nullable
+    private final Class<?> actualType;
 
     public InvalidSerializedObjectException(final Class<?> expected, @Nullable final Object actual) {
         this(expected, actual == null ? null : actual.getClass());
@@ -16,6 +22,8 @@ public class InvalidSerializedObjectException extends RuntimeException {
 
     public InvalidSerializedObjectException(final Class<?> expected, @Nullable final Class<?> actual) {
         super("Invalid serialized object, expected " + expected.getName() + " but got " + (actual == null ? "null" : actual.getName()));
+        this.expectedType = expected;
+        this.actualType = actual;
     }
 
 }
